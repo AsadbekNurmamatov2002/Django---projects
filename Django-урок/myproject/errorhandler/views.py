bb9c660653a28fcbdaeb57631143cd444c3a9777
Asadbek <144318530+AsadbekNurmamatov2002@users.noreply.github.com>
2023-11-20 02:48:28 +0500

    Add files via upload

---
+++ Django-урок/myproject/errorhandler/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import redirect
+# Create your views here.
+ 
+ 
+def error_404_view(request, exception):
+   
+    # we add the path to the handler404.html file
+    # here. The name of our HTML file is 404.html
+
+    # здесь. Имя нашего HTML-файла — 404.html.
+    # добавляем путь к файлу handler404.html
+
+    
+    return render(request, 'handler404.html')
